7f562f999ecf2b9d033bb23f5662c5ad70c7fb8c
MYNLO <pierrenavay@gmail.com>
2026-05-16 00:34:01 +0800

    Delete src directory

--- src/lib/utils/markdown.ts
+++
@@ -1,8 +0,0 @@
-import { marked } from 'marked';
-import DOMPurify from 'dompurify';
-
-export function renderMarkdown(text: string): string {
-  if (!text) return '';
-  const html = marked.parse(text, { breaks: true, gfm: true });
-  return DOMPurify.sanitize(html as string);
-}
